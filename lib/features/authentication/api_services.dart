6b170c4ee12d3f183dc9a838328c31f5da57c946
Md. Ahsanul Alam Naiem <ahsanulnaiem23@gmail.com>
2025-04-08 12:12:16 +0600

    error hanled

--- lib/features/authentication/api_services.dart
+++ lib/features/authentication/api_services.dart
@@ -6,6 +6,7 @@ import 'package:beton_book/core/network_manager/api_end_points.dart';
 import 'package:beton_book/core/network_manager/dio_client.dart';
 import 'package:beton_book/core/presentation/app_provider.dart';
 import 'package:beton_book/features/authentication/provider.dart';
+import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:provider/provider.dart';
@@ -39,34 +40,71 @@ class ApiService{
       }
       return canFetchedUserInfo;
     }
-}
+  }
 
   Future<Map<String, String>?> authenticate({required String email, required String password}) async {
     final body = {"email": email, "password": password};
     localProvider.setLoadingState(true);
-    final response = await dioClient.post(ApiEndPoints.login, data: body);
-    localProvider.setLoadingState(false);
+    try{
+      final response = await dioClient.post(ApiEndPoints.login, data: body);
 
-    if (response.statusCode == 200) {
-      final token = response.data['data']["token"];
-      userMap["id"] = response.data['data']['employee_id'];
-      final csrfToken = response.headers['set-cookie']![0].split(";")[0];
-      final sessionId = response.headers['set-cookie']![1].split(";")[0];
-      final authHeaders = {"cookie": "$csrfToken; $sessionId", "Authorization": "Token $token"};
-      return authHeaders;
+      if (response.statusCode == 200) {
+        final token = response.data['data']["token"];
+        userMap["id"] = response.data['data']['employee_id'];
+        final csrfToken = response.headers['set-cookie']![0].split(";")[0];
+        final sessionId = response.headers['set-cookie']![1].split(";")[0];
+        final authHeaders = {
+          "cookie": "$csrfToken; $sessionId",
+          "Authorization": "Token $token"
+        };
+        return authHeaders;
+      }
+    } on DioException catch (e) {
+      if (e.response != null) {
+        final statusCode = e.response?.statusCode;
+        final data = e.response?.data;
+        if (data is Map<String, dynamic>) {
+          // Server responded error in my request
+          final message = '${data['error']?['message'] ?? "Something went wrong"}\n\n${data['error']?['details'] ?? "Try again later"}';
+        }
+      } else {
+        // Network error, timeout, etc.
+        message= "Something went wrong! Check Internet Connection.";
+      }
+    } catch (e) {
+      message= "System Error.";
+    } finally {
+      localProvider.setLoadingState(false);
     }
   }
 
   Future<bool> fetchUserInfoFunction() async {
     localProvider.setLoadingState(true);
-    final response = await dioClient.get(ApiEndPoints.employeeDetails);
-    localProvider.setLoadingState(false);
-    if (response.statusCode == 200) {
-      Map<String, dynamic> responseJson = response.data.cast<String,dynamic>();
-      userMap.addAll(responseJson);
-      return true;
-    } else{
+    try {
+      final response = await dioClient.get(ApiEndPoints.employeeDetails);
+        Map<String, dynamic> responseJson = response.data.cast<String,
+            dynamic>();
+        userMap.addAll(responseJson);
+        localProvider.setLoadingState(false);
+        return true;
+    }on DioException catch (e) {
+      if (e.response != null) {
+        final statusCode = e.response?.statusCode;
+        final data = e.response?.data;
+        if (data is Map<String, dynamic>) {
+          // Server responded error in my request
+          final message = '${data['error']?['message'] ?? "Something went wrong"}\n\n${data['error']?['details'] ?? "Try again later"}';
+        }
+      } else {
+        // Network error, timeout, etc.
+        message= "Something went wrong! Check Internet Connection.";
+      }
       return false;
+    } catch (e) {
+      message= "System Error.";
+      return false;
+    } finally {
+      localProvider.setLoadingState(false);
     }
   }
 
@@ -80,4 +118,6 @@ class ApiService{
       return companyResponseJson;
     }}
 }
+
+
 
